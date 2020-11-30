674f03565b3c71a0f6b93693f12cee84463a3fa3
migueldgomes <miguel.d.gomes@gmail.com>
2020-11-30 18:26:38 +0000

    Add purchases and fixes in contract

--- market-dapp/backend/contracts/IPFSInbox.sol
+++ market-dapp/backend/contracts/IPFSInbox.sol
@@ -4,6 +4,7 @@ pragma experimental ABIEncoderV2;
 contract IPFSInbox {    
     
     struct carSetup {
+        uint256 itemId;
         string ipfsHash;
         string carBrand;
         string track;
@@ -14,70 +15,94 @@ contract IPFSInbox {
     }
     
     struct skin {
+        uint256 itemId;
         string ipfsHash;
         string carBrand;
         string simulator;
         uint256 price;
         string _address;
     }
+
+    struct purchase {
+        uint256 purchaseId;
+        uint256 itemId;
+        address buyer;
+    }
     
+    // Maps the 2 type of files
+    mapping (address => carSetup[]) setupsBySeller;
+    mapping (address => skin[]) skinsBySeller;
     
-    //Saves those 2 files
-    mapping (address => carSetup[]) carsInbox;
-    mapping (address => skin[]) skinsInbox;
-    
-    
-    //To track if vendor address already exists
+    // To track if seller address already exists
     mapping (address => bool) userExists;
     
-    //Keep track of all vendor addresses and existing files
+    // Keep track of all seller addresses and existing files
     address[] private userAddresses;
     string[] private ipfsList;
-    uint256 public carsCounter;
+
+    uint256 public carSetupCounter;
     uint256 public skinsCounter;
-        
+    uint256 public purchaseCounter;
+    uint256 public itemCounter;
+
+    mapping (address => purchase[]) purchasesBySeller;
+
     // Events
     event ipfsSaved(string _ipfsHash, address _address);
-    event carSaved(address _address, string _ipfsHash, string _carBrand, string _track, string _simulator, string _season, uint256 _price);
+    event carSetupSaved(address _address, string _ipfsHash, string _carBrand, string _track, string _simulator, string _season, uint256 _price);
     event skinSaved(address _address, string _ipfsHash, string _carBrand, string _simulator, uint256 _price);
+    event newPurchaseRequest(address _address, uint256 itemId);
     
-    // An empty constructor that creates an instance of the contract
+    /// @notice An empty constructor that creates an instance of the contract
     constructor() public{
-        carsCounter = 0;
+        carSetupCounter = 0;
         skinsCounter = 0;
     }    
     
-
-    function saveCar(address _address, string memory _ipfsHash, string memory _carBrand, string memory _track, string memory _simulator, string memory _season, uint256 _price) public {
-        string memory string_address = addressToString(_address);
-        carSetup memory car = carSetup(_ipfsHash, _carBrand, _track, _simulator, _season, _price, string_address);
-        carsInbox[_address].push(car);
-        carsCounter++;
+    /// @notice registers a new car setup for sale
+    function newCarSetup(string memory _ipfsHash, string memory _carBrand, string memory _track, string memory _simulator, string memory _season, uint256 _price) public {
+        string memory string_address = addressToString(msg.sender);
+        carSetup memory car = carSetup(itemCounter, _ipfsHash, _carBrand, _track, _simulator, _season, _price, string_address);
+        itemCounter++;
+        setupsBySeller[msg.sender].push(car);
+        carSetupCounter++;
         ipfsList.push(_ipfsHash);
-        emit carSaved(_address,_ipfsHash, _carBrand, _track, _simulator, _season, _price);
-        if(userExists[_address] == false) {
-            userExists[_address] = true;
-            userAddresses.push(_address);    
+        emit carSetupSaved(msg.sender,_ipfsHash, _carBrand, _track, _simulator, _season, _price);
+        if(userExists[msg.sender] == false) {
+            userExists[msg.sender] = true;
+            userAddresses.push(msg.sender);    
         }
     }
     
-
-    
-    
-    function saveSkin(address _address, string memory _ipfsHash, string memory _carBrand, string memory _simulator, uint256 _price) public {
-        string memory string_address = addressToString(_address);
-        skin memory newSkin = skin(_ipfsHash, _carBrand,  _simulator, _price, string_address);
-        skinsInbox[_address].push(newSkin);
+    /// @notice registers a new car skin for sale
+    function newSkin(string memory _ipfsHash, string memory _carBrand, string memory _simulator, uint256 _price) public {
+        string memory string_address = addressToString(msg.sender);
+        skin memory carSkin = skin(itemCounter, _ipfsHash, _carBrand,  _simulator, _price, string_address);
+        itemCounter++;
+        skinsBySeller[msg.sender].push(carSkin);
         skinsCounter++;
         ipfsList.push(_ipfsHash);
-        emit skinSaved (_address,_ipfsHash, _carBrand, _simulator, _price);
-        if(userExists[_address] == false) {
-            userExists[_address] = true;
-            userAddresses.push(_address);    
+        emit skinSaved (msg.sender,_ipfsHash, _carBrand, _simulator, _price);
+        if(userExists[msg.sender] == false) {
+            userExists[msg.sender] = true;
+            userAddresses.push(msg.sender);    
         }
     }
 
-    function saveVendor(address _address) public returns(bool){
+    /// @notice registers a new purchase request
+    function purchaseRequest(uint256 itemId) public returns(uint256) {
+        uint256 purchaseId = purchaseCounter;
+        purchaseCounter++;
+        purchase memory newPurchase = purchase(purchaseCounter, itemId, msg.sender);
+        
+        purchasesBySeller[msg.sender].push(newPurchase);
+
+        emit newPurchaseRequest(msg.sender,itemId);
+
+        return purchaseId;
+    }
+
+    function saveSeller(address _address) public returns(bool){
         if(userExists[_address] == false) {
             userExists[_address] = true;
             userAddresses.push(_address);
@@ -86,14 +111,14 @@ contract IPFSInbox {
         return false;
     }
     
-    function getCars() public view returns(carSetup[] memory allCars){
-        carSetup[] memory cars = new carSetup[](carsCounter);
+    function getCarSetups() public view returns(carSetup[] memory allCars){
+        carSetup[] memory cars = new carSetup[](carSetupCounter);
         uint256 i = 0;
         for(uint256 j = 0; j < userAddresses.length; j++) {
             address _address =  userAddresses[j];
             
-            for(uint256 k = 0; k < carsInbox[_address].length; k++) {
-                carSetup storage car = carsInbox[_address][k];
+            for(uint256 k = 0; k < setupsBySeller[_address].length; k++) {
+                carSetup storage car = setupsBySeller[_address][k];
                 cars[i] = car;
                 i++;
             }
@@ -107,8 +132,8 @@ contract IPFSInbox {
         for(uint256 j = 0; j < userAddresses.length; j++) {
             address _address =  userAddresses[j];
             
-            for(uint256 k = 0; k < skinsInbox[_address].length; k++) {
-                skin storage skin2 = skinsInbox[_address][k];
+            for(uint256 k = 0; k < skinsBySeller[_address].length; k++) {
+                skin storage skin2 = skinsBySeller[_address][k];
                 skins[i] = skin2;
                 i++;
             }
@@ -116,17 +141,16 @@ contract IPFSInbox {
         return skins;
     }
 
-    function isVendor(address _address) public view returns(bool) {
+    function isSeller(address _address) public view returns(bool) {
         return userExists[_address];
     }
     
-    
-    function getNumberVendors() public view returns(uint256) {
+    function getNumberSellers() public view returns(uint256) {
         return userAddresses.length;
     }
     
     function getNumberCars() public view returns(uint256) {
-        return carsCounter;
+        return carSetupCounter;
     }
     
     function getNumberSkins() public view returns(uint256) {
