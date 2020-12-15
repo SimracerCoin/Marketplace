3a716a9d9da027c36bf7ec5db4183e50eeafcddc
migueldgomes <miguel.d.gomes@gmail.com>
2020-12-15 20:11:16 +0000

    Update migrations

--- market-dapp/backend/migrations/2_deploy_ipfscontract.js
+++ market-dapp/backend/migrations/2_deploy_ipfscontract.js
@@ -1,5 +1,5 @@
 var STMarketplace = artifacts.require("./STMarketplace.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(STMarketplace);
+  deployer.deploy(STMarketplace, '0x5660a2Ead35A975D9F7C96d0B5f5FA12A12710d8');
 };
