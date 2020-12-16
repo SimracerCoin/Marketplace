c7d9a581eda4d427c218731beb3de186309b388e
migueldgomes <miguel.d.gomes@gmail.com>
2020-12-16 16:32:15 +0000

    Fixes to work with the new contract

--- market-dapp/backend/migrations/2_deploy_ipfscontract.js
+++ market-dapp/backend/migrations/2_deploy_ipfscontract.js
@@ -1,5 +1,7 @@
 var STMarketplace = artifacts.require("./STMarketplace.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(STMarketplace, '0x5660a2Ead35A975D9F7C96d0B5f5FA12A12710d8');
+  //TODO: change placeholder address for the real deployed Descartes address
+  const placeholder = '0x5660a2Ead35A975D9F7C96d0B5f5FA12A12710d8';
+  deployer.deploy(STMarketplace, placeholder);
 };
