757205076c1914e0f434e34ec5837d30e76d9344
Carlos Martins <c.carlos.martins@gmail.com>
2021-02-21 19:20:59 +0000

    fix script to cartesi-machine decryt the file

--- market-dapp/backend/testmachine/decrypt.sh
+++ market-dapp/backend/testmachine/decrypt.sh
@@ -1,27 +1,18 @@
 #
-# sample: /decrypt.sh "jA0ECQMCTK5jjrZOgnj20j8Bbd6Lt03v6DvZttqfWIG6gzGd/aCUw8qTwYdxkxqExiv0JwdvzoQp4M1TdkxNZxVOCJwR3qpJqduW4m8Zs2k=" 12345Abc
-# dependency: https://www.cyberciti.biz/tips/linux-how-to-encrypt-and-decrypt-files-with-a-password.html
+# sample: /decrypt.sh /path/to/encrypted_file /path/to/password_file 
 #
-ENCODED_FILE=$1
-PASSWORD=$2
-# check if command exists and fail otherwise
-command -v "gpg" >/dev/null 2>&1
-if [[ $? -ne 0 ]]; then
-    echo "I require GPG but it's not installed. Abort."
-    exit 1
-fi
-FILE=`echo -n $ENCODED_FILE | base64 -d`          #decode file
-tmpfile_in=$(mktemp decrypt-script.XXXXXX.gpg)          #create temp file to store encrypted file
-tmpfile_out=${tmpfile_in/".gpg"/""}
-echo "$FILE" >> "$tmpfile_in"
-gpg --batch --passphrase "$PASSWORD" -o "$tmpfile_out" -d "$tmpfile_in" 2>/dev/null #decrypt file
+ENCRYPTED_FILE=$1
+PASSWORD_FILE=$2
+
+[ ! -f $ENCRYPTED_FILE ] && { echo "$ENCRYPTED_FILE file not found"; exit 99; }
+[ ! -f $PASSWORD_FILE ] && { echo "$PASSWORD_FILE file not found"; exit 99; }
+
+password=$(tr -d '\0' <$PASSWORD_FILE)              #read string for binary file                 
+
+tmpfile_out=$(mktemp -u)                            #create temp file to store decrypted file
+
+gpg -d --batch --passphrase "$password" -o $tmpfile_out $ENCRYPTED_FILE  #decrypt file
+
 # check if decrypt went well
-if [[ ! -f "$tmpfile_out" ]]; then
-    rm "$tmpfile_in" 
-    echo "decryption error"
-    exit 1
-fi
-cat "$tmpfile_out"  #script output decrypted file
-#remove tmp file
-rm "$tmpfile_in" 
-rm "$tmpfile_out"
+[ ! -s $tmpfile_out ] && { echo "decryption error"; exit 99; }
+cat $tmpfile_out  #script output decrypted file
