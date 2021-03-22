4936ed26dbf1641515cb13feb0600bd8783fc4be
AfonsoMiao <afonsomeow@gmail.com>
2021-03-22 00:24:09 +0000

    Fixed error in ipfs

--- market-dapp/client/src/ipfs.js
+++ market-dapp/client/src/ipfs.js
@@ -3,8 +3,7 @@ const IPFS = require('ipfs-http-client');
 const ipfs = new IPFS({ 
     host: 'ipfs.infura.io', 
     port: 5001, 
-    protocol: 'https',
-    headers: {'Access-Control-Allow-Origin': '*'}
+    protocol: 'https'
 });
 
 export default ipfs;
