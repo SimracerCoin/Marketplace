984ce2261e420d9c9eb1fa8561510929bb1ac604
migueldgomes <miguel.d.gomes@gmail.com>
2021-03-29 11:47:59 +0100

    New RegisterNFT page

---
+++ market-dapp/client/src/pages/RegisterNFT.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import { Dropdown, Form, DropdownButton, Button } from 'react-bootstrap';
+import { Prompt } from 'react-st-modal';
+
+
+class RegisterNFT extends Component {
+
+}
