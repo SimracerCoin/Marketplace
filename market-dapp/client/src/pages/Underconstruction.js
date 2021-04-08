3c12eb1c150618803d7222a7fddcd58f64d041db
Carlos Martins <c.carlos.martins@gmail.com>
2021-04-08 04:45:26 +0100

    last improvements

--- market-dapp/client/src/pages/Underconstruction.js
+++ market-dapp/client/src/pages/Underconstruction.js
@@ -21,6 +21,10 @@ class Underconstruction extends Component {
         this.setState({ currentAccount: currentAccount });
     }
 
+    requestBtnClick = (event) => {
+        document.querySelector('.discord-dialog').className = 'discord-dialog discord-dialog--active';
+    }
+
     render() {
         return ([
                 <header id="header" class="header h-fullscreen__page text-light">
@@ -32,8 +36,8 @@ class Underconstruction extends Component {
                             <div class="col-lg-6 mx-auto">
                             <div class="fadeIn ad-500ms">
                                 <h1 class="slideInLeft ad-400ms display-lg-2 fw-700 lh-2 mb-4"><span class="text-warning">Sim</span>thunder</h1>
-                                <h2 class="lead-2 ls-3 d-block slideInRight ad-500ms fw-300 text-uppercase mb-7">Our new site is coming soon!!!</h2>
-                                <p class="lead-lg mb-7">Stay tuned for something amazing</p>
+                                <h2 class="lead-2 ls-3 d-block slideInRight ad-500ms fw-300 text-uppercase mb-7">Beta 1.0</h2>
+                                <a class="btn btn-lg btn-round btn-outline-light" onClick={this.requestBtnClick}>Request Beta Access</a>
                             </div>
                             </div>
                         </div>
