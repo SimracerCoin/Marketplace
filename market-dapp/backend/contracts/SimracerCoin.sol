85b2f1053d6836f86e5550a0f68db90bec5e3719
Paulo Cristo <cristo.paulo@gmail.com>
2022-06-30 15:33:12 +0100

    add comments for deploy on remix

--- market-dapp/backend/contracts/SimracerCoin.sol
+++ market-dapp/backend/contracts/SimracerCoin.sol
@@ -4,6 +4,12 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+//for building on Remix:
+//import "@openzeppelin/contracts@3.1.0-solc-0.7/token/ERC20/ERC20.sol";
+//import "@openzeppelin/contracts@3.1.0-solc-0.7/token/ERC20/ERC20Burnable.sol";
+//import "@openzeppelin/contracts@3.1.0-solc-0.7/access/Ownable.sol";
+
+
 contract SimracerCoin is ERC20, ERC20Burnable, Ownable {
     uint private INITIAL_SUPPLY = 200000000e18;
 
