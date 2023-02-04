c8347e32fdcc178a7dee997c8a86303a5cb7c01d
Carlos Martins <c.carlos.martins@gmail.com>
2023-02-04 11:23:02 +0000

    add faqs

--- market-dapp/client/src/pages/FaqsPage.js
+++ market-dapp/client/src/pages/FaqsPage.js
@@ -14,7 +14,14 @@ class FaqsPage extends Component {
                                     <h2 class="ls-1 text-center">FAQs</h2>
                                     <hr class="w-10 border-warning border-top-2 o-90" />
 
-                                    <h3 class="ls-1 text-center">Coming soon</h3>
+                                    <h5>How can I buy items in the marketplace?</h5>
+                                    <p class="lead-1">To buy items you need first to have Simracer Coin (SRC).</p>
+
+                                    <h5>How can I buy Simracer Coin (SRC)?</h5>
+                                    <p class="lead-1">Simracer Coin is available in Catex exchange (https://catex.io).</p>
+
+                                    <h5>What are Ownership NFTs?</h5>
+                                    <p class="lead-1">These are like entries for private league competitions. By having the NFT you are owner of that car entry and number and receive the prize if that car gets one.</p>
 
 {/*
                                     <h5>#1 - Don't do that</h5>
