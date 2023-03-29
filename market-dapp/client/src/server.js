6df0ad58aede7f3b9f86b0ad2094fabee77915c0
Carlos Martins <c.carlos.martins@gmail.com>
2023-03-29 01:45:39 +0100

    fix server.js

--- market-dapp/client/src/server.js
+++ market-dapp/client/src/server.js
@@ -2,23 +2,21 @@
 
 const express = require('express');
 const path = require('path');
-const http = require('http');
 const https = require('https');
 const fs = require('fs');
 const app = express();
 const knex = require('./db');
-const cors = require('cors');
 
 require('dotenv').config();
 
 const PORT = process.env.PORT || 443;
 const INDEX = path.join(__dirname, '..', 'build', 'index.html'); // Initialization.
 
-const httpsServer = PORT === 443 ? https.createServer({
+const httpsServer = https.createServer({
 	key: fs.readFileSync('/etc/letsencrypt/live/simthunder.com/privkey.pem'),
 	cert: fs.readFileSync('/etc/letsencrypt/live/simthunder.com/cert.pem'),
 	ca: fs.readFileSync('/etc/letsencrypt/live/simthunder.com/chain.pem')
-}, app) : http.createServer(app); // define routes and socket
+}, app); // define routes and socket
 
 app.use(express.static(
 	path.join(__dirname, '..','build'), 
@@ -65,24 +63,3 @@ app.get('*', (req, res) => {
 });
 
 httpsServer.listen(PORT);
-
-if(PORT === 443) {
-	// ========================================== //
-	// =========== LISTEN PORT 80 =============== //
-	const http_app = express();
-
-	/*http_app.use('/', express.static(path.join(__dirname, '../dist/'), {
-	dotfiles: 'allow'
-	}));*/ //let requestHandler = app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
-
-	http_app.get('*', function (req, res) {
-		res.redirect('https://' + req.headers.host + req.url);
-	}); // have it listen on 80
-
-	http.createServer(http_app).listen(80); // set up a route to redirect http to https
-	// ========================================== //
-} else {
-	app.use(cors({
-		origin: '*'
-	}));
-}
