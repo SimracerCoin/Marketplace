f36e9450a3c1bb48576f729aac421179f8b2d263
Carlos Martins <c.carlos.martins@gmail.com>
2024-01-30 15:15:23 +0000

    fix transfer fee to owner

--- market-dapp/backend/contracts/ContentMarketplaceTypes.sol
+++ market-dapp/backend/contracts/ContentMarketplaceTypes.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.7.0;
 
 enum PurchaseStatus { Request, Accept_A, Challenge, Accept_B, Reject }
--- market-dapp/backend/deploy/01_contracts.ts
+++ market-dapp/backend/deploy/01_contracts.ts
@@ -9,7 +9,9 @@ const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
   //const Descartes = await get("Descartes");
 
   //NOTE: if deploying to polygon use instead
-  //const SimracerCoin = {address: "0xf0c3C4AC63Be272a94712bCcc39490A159Cd0D7C"};
+  /*const SimracerCoin = {address: "0xf0c3C4AC63Be272a94712bCcc39490A159Cd0D7C"};
+  const STSetup = {address: "0x4AffCCa2174dC8BC3A841B65578CFe6f7724420f"};
+  const STSkin = {address: "0x48abadA0E4f651b16AdE669C2757d61A6768Fb55"};*/
   //and then npx hardhat deploy --network polygon
   const SimracerCoin = await deploy("SimracerCoin", {
     from: deployer,
@@ -26,12 +28,7 @@ const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
     log: true,
     args: [SimracerCoin.address],
   });
-
-  /*await deploy("ContentMarketplace", {
-    from: deployer,
-    log: true,
-    args: [SimracerCoin.address],
-  });*/
+  
   const STSetup = await deploy("STSetup", {
     from: deployer,
     log: true
